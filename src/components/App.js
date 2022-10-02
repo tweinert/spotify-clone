6fd480398a9eeac1610512c414f5b25e0b2a694b
Tait Weinert <tait.weinert@outlook.com>
2022-10-02 12:25:06 +1030

    styling adjustments

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Styles from "../styles/app.module.css";
 import Nav from "./Nav/Nav";
 import Home from "./Home/Home";
 import Footer from "./Footer/Footer";
 
+
 function App() {
   return (
     <BrowserRouter>
-      <div className="app">
+      <div className={Styles.app}>
         <Nav />
         <Routes>
           {/* <Route path="/search"><Search /></Route> */}
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,35 @@
 import Styles from "../../styles/home/home.module.css";
 
 function Home() {
-  return <div className={Styles.home}>This is Home</div>;
+  return (
+    <div className={Styles.home}>
+      <ul>
+        <li>Test</li>
+        <li>Test</li>
+        <li>Test</li>
+        <li>Test</li>
+        <li>Test</li>
+        <li>Test</li>
+        <li>Test</li>
+        <li>Test</li>
+        <li>Test</li>
+        <li>Test</li>
+        <li>Test</li>
+        <li>Test</li>
+        <li>Test</li>
+        <li>Test</li>
+        <li>Test</li>
+        <li>Test</li>
+        <li>Test</li>
+        <li>Test</li>
+        <li>Test</li>
+        <li>Test</li>
+        <li>Test</li>
+        <li>Test</li>
+        <li>Test</li>
+      </ul>
+    </div>
+  );
 }
 
 export default Home;
