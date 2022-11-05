43113f26ea58da20d3ec203049ceeb87106606ff
Tait Weinert <tait.weinert@outlook.com>
2022-11-05 10:30:47 +1030

    main styling changes

--- src/components/Home/RecentlyPlayed.js
+++ src/components/Home/RecentlyPlayed.js
@@ -3,7 +3,30 @@ import Styles from "../../styles/home/recentlyPlayed.module.css"
 function RecentlyPlayed() {
   return (
     <div className={Styles.mainContainer}>
-
+      <div className={Styles.recentContainer}>
+        <img className={Styles.recentImage}></img>
+        <span className={Styles.recentText}></span>
+      </div>
+      <div className={Styles.recentContainer}>
+        <img className={Styles.recentImage}></img>
+        <span className={Styles.recentText}></span>
+      </div>
+      <div className={Styles.recentContainer}>
+        <img className={Styles.recentImage}></img>
+        <span className={Styles.recentText}></span>
+      </div>
+      <div className={Styles.recentContainer}>
+        <img className={Styles.recentImage}></img>
+        <span className={Styles.recentText}></span>
+      </div>
+      <div className={Styles.recentContainer}>
+        <img className={Styles.recentImage}></img>
+        <span className={Styles.recentText}></span>
+      </div>
+      <div className={Styles.recentContainer}>
+        <img className={Styles.recentImage}></img>
+        <span className={Styles.recentText}></span>
+      </div>
     </div>
   );
 }
