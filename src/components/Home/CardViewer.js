d593d17ea7bf3998b211008836a3dc1fbd502080
Tait Weinert <tait.weinert@outlook.com>
2022-11-17 18:33:13 +1030

    added cardViewer title

--- src/components/Home/CardViewer.js
+++ src/components/Home/CardViewer.js
@@ -2,36 +2,39 @@ import Styles from "../../styles/home/cardViewer.module.css";
 
 function CardViewer() {
   return (
-    <div className={Styles.mainContainer}>
-      <div className={Styles.cardContainer}>
-        <img className={Styles.cardImage}></img>
-        <span className={Styles.cardHeader}>Discover Weekly</span>
-        <span className={Styles.cardText}>Your weekly mixtape of fresh music</span>
-      </div>
-      <div className={Styles.cardContainer}>
-        <img className={Styles.cardImage}></img>
-        <span className={Styles.cardHeader}>Discover Weekly</span>
-        <span className={Styles.cardText}>Your weekly mixtape of fresh music</span>
-      </div>
-      <div className={Styles.cardContainer}>
-        <img className={Styles.cardImage}></img>
-        <span className={Styles.cardHeader}>Discover Weekly</span>
-        <span className={Styles.cardText}>Your weekly mixtape of fresh music</span>
-      </div>
-      <div className={Styles.cardContainer}>
-        <img className={Styles.cardImage}></img>
-        <span className={Styles.cardHeader}>Discover Weekly</span>
-        <span className={Styles.cardText}>Your weekly mixtape of fresh music</span>
-      </div>
-      <div className={Styles.cardContainer}>
-        <img className={Styles.cardImage}></img>
-        <span className={Styles.cardHeader}>Discover Weekly</span>
-        <span className={Styles.cardText}>Your weekly mixtape of fresh music</span>
-      </div>
-      <div className={Styles.cardContainer}>
-        <img className={Styles.cardImage}></img>
-        <span className={Styles.cardHeader}>Discover Weekly</span>
-        <span className={Styles.cardText}>Your weekly mixtape of fresh music</span>
+    <div>
+      <h2 className={Styles.title}>Your Top Mixes</h2>
+      <div className={Styles.mainContainer}>
+        <div className={Styles.cardContainer}>
+          <img className={Styles.cardImage}></img>
+          <span className={Styles.cardHeader}>Discover Weekly</span>
+          <span className={Styles.cardText}>Your weekly mixtape of fresh music</span>
+        </div>
+        <div className={Styles.cardContainer}>
+          <img className={Styles.cardImage}></img>
+          <span className={Styles.cardHeader}>Discover Weekly</span>
+          <span className={Styles.cardText}>Your weekly mixtape of fresh music</span>
+        </div>
+        <div className={Styles.cardContainer}>
+          <img className={Styles.cardImage}></img>
+          <span className={Styles.cardHeader}>Discover Weekly</span>
+          <span className={Styles.cardText}>Your weekly mixtape of fresh music</span>
+        </div>
+        <div className={Styles.cardContainer}>
+          <img className={Styles.cardImage}></img>
+          <span className={Styles.cardHeader}>Discover Weekly</span>
+          <span className={Styles.cardText}>Your weekly mixtape of fresh music</span>
+        </div>
+        <div className={Styles.cardContainer}>
+          <img className={Styles.cardImage}></img>
+          <span className={Styles.cardHeader}>Discover Weekly</span>
+          <span className={Styles.cardText}>Your weekly mixtape of fresh music</span>
+        </div>
+        <div className={Styles.cardContainer}>
+          <img className={Styles.cardImage}></img>
+          <span className={Styles.cardHeader}>Discover Weekly</span>
+          <span className={Styles.cardText}>Your weekly mixtape of fresh music</span>
+        </div>
       </div>
     </div>
   );
