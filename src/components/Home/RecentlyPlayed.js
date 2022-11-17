b20943eb77ed8862fea8d5682911c12165041d5d
Tait Weinert <tait.weinert@outlook.com>
2022-11-17 18:47:17 +1030

    added recentlyPlayed title

--- src/components/Home/RecentlyPlayed.js
+++ src/components/Home/RecentlyPlayed.js
@@ -2,30 +2,33 @@ import Styles from "../../styles/home/recentlyPlayed.module.css"
 
 function RecentlyPlayed() {
   return (
-    <div className={Styles.mainContainer}>
-      <div className={Styles.recentContainer}>
-        <img className={Styles.recentImage}></img>
-        <span className={Styles.recentText}>Example Playlist Name</span>
-      </div>
-      <div className={Styles.recentContainer}>
-        <img className={Styles.recentImage}></img>
-        <span className={Styles.recentText}>Example Playlist Name</span>
-      </div>
-      <div className={Styles.recentContainer}>
-        <img className={Styles.recentImage}></img>
-        <span className={Styles.recentText}>Example Playlist Name</span>
-      </div>
-      <div className={Styles.recentContainer}>
-        <img className={Styles.recentImage}></img>
-        <span className={Styles.recentText}>Example Playlist Name</span>
-      </div>
-      <div className={Styles.recentContainer}>
-        <img className={Styles.recentImage}></img>
-        <span className={Styles.recentText}>Example Playlist Name</span>
-      </div>
-      <div className={Styles.recentContainer}>
-        <img className={Styles.recentImage}></img>
-        <span className={Styles.recentText}>Example Playlist Name</span>
+    <div>
+      <h1 className={Styles.title}>Good (Morning/Afternoon/Evening)</h1>
+      <div className={Styles.mainContainer}>
+        <div className={Styles.recentContainer}>
+          <img className={Styles.recentImage}></img>
+          <span className={Styles.recentText}>Example Playlist Name</span>
+        </div>
+        <div className={Styles.recentContainer}>
+          <img className={Styles.recentImage}></img>
+          <span className={Styles.recentText}>Example Playlist Name</span>
+        </div>
+        <div className={Styles.recentContainer}>
+          <img className={Styles.recentImage}></img>
+          <span className={Styles.recentText}>Example Playlist Name</span>
+        </div>
+        <div className={Styles.recentContainer}>
+          <img className={Styles.recentImage}></img>
+          <span className={Styles.recentText}>Example Playlist Name</span>
+        </div>
+        <div className={Styles.recentContainer}>
+          <img className={Styles.recentImage}></img>
+          <span className={Styles.recentText}>Example Playlist Name</span>
+        </div>
+        <div className={Styles.recentContainer}>
+          <img className={Styles.recentImage}></img>
+          <span className={Styles.recentText}>Example Playlist Name</span>
+        </div>
       </div>
     </div>
   );
