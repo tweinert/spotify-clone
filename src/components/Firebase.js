7d997069195ea8aa6a6d1654c61117097dd777ae
Tait Weinert <tait.weinert@outlook.com>
2022-11-20 11:21:25 +1030

    added firebase

---
+++ src/components/Firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB3ONqntTxMWJ3Br898lwtocVsedrqwXCw",
+  authDomain: "spotify-clone-eab01.firebaseapp.com",
+  projectId: "spotify-clone-eab01",
+  storageBucket: "spotify-clone-eab01.appspot.com",
+  messagingSenderId: "679841981447",
+  appId: "1:679841981447:web:64adbcbc914a1385e176a2"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
