65bb6d36f891f6e875f0df47c85a840f58461d68
Tait Weinert <tait.weinert@outlook.com>
2022-12-18 12:30:59 +1030

    finished artistPage player styling

--- src/components/ArtistPage/ArtistPage.js
+++ src/components/ArtistPage/ArtistPage.js
@@ -1,11 +1,12 @@
 import Styles from "../../styles/artistPage/artistPage.module.css";
 import Header from "./Header.js";
+import Player from "./Player";
 
 function ArtistPage() {
   return (
     <div className={Styles.artistPage}>
       <Header />
-      {/* <Player /> */}
+      <Player />
       {/* <SongList /> */}
     </div>
   );
--- src/components/ArtistPage/Player.js
+++ src/components/ArtistPage/Player.js
@@ -3,7 +3,7 @@ import Styles from "../../styles/artistPage/player.module.css"
 function Player() {
   return (
     <div className={Styles.player}>
-      
+      <button className={Styles.playButton}>Play</button>
     </div>
   );
 }
