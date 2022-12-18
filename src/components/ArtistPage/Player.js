777c83b15c64e3b5667e7684e0ba853c759d7104
Tait Weinert <tait.weinert@outlook.com>
2022-12-18 12:02:42 +1030

    created artistPage player

---
+++ src/components/ArtistPage/Player.js
@@ -0,0 +1,11 @@
+import Styles from "../../styles/artistPage/player.module.css"
+
+function Player() {
+  return (
+    <div className={Styles.player}>
+      
+    </div>
+  );
+}
+
+export default Player;
