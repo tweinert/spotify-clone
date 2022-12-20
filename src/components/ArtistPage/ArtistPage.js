874907c4738cd47eb0baedcfd1eabf98d6e6c8ed
Tait Weinert <tait.weinert@outlook.com>
2022-12-20 18:49:40 +1030

    created songList

--- src/components/ArtistPage/ArtistPage.js
+++ src/components/ArtistPage/ArtistPage.js
@@ -1,13 +1,14 @@
 import Styles from "../../styles/artistPage/artistPage.module.css";
 import Header from "./Header.js";
 import Player from "./Player";
+import SongList from "./SongList";
 
 function ArtistPage() {
   return (
     <div className={Styles.artistPage}>
       <Header />
       <Player />
-      {/* <SongList /> */}
+      <SongList />
     </div>
   );
 }
---
+++ src/components/ArtistPage/SongList.js
@@ -0,0 +1,11 @@
+import Styles from "../../styles/artistPage/songList.module.css"
+
+function SongList() {
+  return (
+    <div className={Styles.songList}>
+      This is the song list.
+    </div>
+  );
+}
+
+export default SongList;
