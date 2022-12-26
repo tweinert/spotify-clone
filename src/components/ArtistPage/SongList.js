21952d0944119a76e9218b1d8e189cf4cedb9483
Tait Weinert <tait.weinert@outlook.com>
2022-12-27 09:38:44 +1030

    added songList to artistPage

--- src/components/ArtistPage/SongList.js
+++ src/components/ArtistPage/SongList.js
@@ -4,10 +4,25 @@ function SongList() {
   return (
     <div className={Styles.songList}>
       <div className={Styles.tableHeader}>
-        
+        <div>#</div>
+        <div>Title</div>
+        <div>Album</div>
+        <div>Length</div>
       </div>
+      <hr />
       <div className={Styles.tableSongs}>
-
+        <div className={Styles.tableRow}>
+          <div>1</div>
+          <div>Speak to Me</div>
+          <div>The Dark Side of the Moon</div>
+          <div>1:05</div>
+        </div>
+        <div className={Styles.tableRow}>
+          <div>2</div>
+          <div>Breathe (In the Air)</div>
+          <div>The Dark Side of the Moon</div>
+          <div>2:49</div>
+        </div>
       </div>
     </div>
   );
