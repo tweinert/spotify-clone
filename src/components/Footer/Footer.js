53f83ba6c2c368001d2bf81e8e03abd9b9c1ffee
Tait Weinert <tait.weinert@outlook.com>
2023-02-21 12:50:09 +1030

    song timer start

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,12 +2,43 @@ import { useState, useEffect } from 'react';
 import Styles from "../../styles/footer/footer.module.css";
 
 function Footer(props) {
-  // remove queue[0] when song ends
+  const [songFullTime, setSongFullTime] = useState("00:00");
+  const [songCurrentTime, setSongCurrentTime] = useState("00:00");
   
-  // props.queue[0].Title
+  // on song queue change
+  useEffect(() => {
+    // set new time
+    let songTime = props.isPlaying ? props.queue[0].Length : "0";
+    setSongFullTime(songTime);
+    setSongCurrentTime(songTime);
+
+    startTimer(songFullTime);
+  }, [props.queue]);
+
+  const getTimeRemaining = (e) => {
+    const total = e;
+    const minutes = Math.floor(total / 60);
+    const seconds = total - minutes * 60;
+    return {
+      total, minutes, seconds
+    };
+  }
+
+  const startTimer = (e) => {
+    let { total, minutes, seconds } = getTimeRemaining(e);
+
+    if (total >= 0) {
+      setSongCurrentTime(
+        (minutes > 9 ? minutes : "0" + minutes) + ":"
+        + (seconds > 9 ? seconds : "0" + seconds)
+      );
+    }
+  }
+  
   return (
     <div className={Styles.footer}>
-      Currently Playing: {props.isPlaying ? props.queue[0].Title : "nothing"}
+      <div>Currently Playing: {props.isPlaying ? props.queue[0].Title : "nothing"}</div>
+      <div>{songCurrentTime}</div>
     </div>
   );
 }
