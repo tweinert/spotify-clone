261fcc96303093692dacd3161aa30867951bf5f7
Tait Weinert <tait.weinert@outlook.com>
2023-03-09 19:39:30 +1030

    moved song timer to footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import Styles from "../../styles/footer/footer.module.css";
 import SongInformation from "./SongInformation";
 import PlayerControls from "./PlayerControls";
@@ -12,9 +13,40 @@ import SongProgressBar from "./SongProgressBar";
   time display bar
 */
 function Footer(props) {
+  const [songFullTime, setSongFullTime] = useState("00:00");
+  const [songCurrentTime, setSongCurrentTime] = useState("00:00");
+
+  // on song queue change
+  useEffect(() => {
+    // set new time
+    let songTime = props.isPlaying ? props.queue[0].Length : "0";
+    setSongFullTime(songTime);
+
+    let interval;
+    if (props.isPlaying) {
+      interval = startTimer(songTime);
+    }
+
+    return () => clearInterval(interval);
+  }, [props.queue]);
+
+  const startTimer = (time) => {
+    setSongCurrentTime(0);
+
+    const timerInterval = setInterval(() => {
+      incrementSongTime();
+    }, 1000);
+
+    return timerInterval;
+  }
+
+  const incrementSongTime = () => {
+    setSongCurrentTime((songCurrentTime) => songCurrentTime + 1);
+  }
+
   return (
     <div className={Styles.footer}>
-      <SongInformation queue={props.queue} setQueue={props.setQueue} isPlaying={props.isPlaying} />
+      <SongInformation queue={props.queue} setQueue={props.setQueue} isPlaying={props.isPlaying} songFullTime={songFullTime} songCurrentTime={songCurrentTime} />
       <PlayerControls />
       <SongProgressBar />
     </div>
--- src/components/Footer/SongInformation.js
+++ src/components/Footer/SongInformation.js
@@ -2,44 +2,6 @@ import { useState, useEffect } from 'react';
 import Styles from "../../styles/footer/songInformation.module.css";
 
 function SongInformation(props) {
-
-  const [songFullTime, setSongFullTime] = useState("00:00");
-  const [songCurrentTime, setSongCurrentTime] = useState("00:00");
-
-  // on song queue change
-  useEffect(() => {
-    // set new time
-    let songTime = props.isPlaying ? props.queue[0].Length : "0";
-    setSongFullTime(songTime);
-
-    let interval = startTimer(songTime);
-
-    return () => clearInterval(interval);
-  }, [props.queue]);
-
-  const getTimeRemaining = (e) => {
-    const total = e;
-    const minutes = Math.floor(total / 60);
-    const seconds = total - minutes * 60;
-    return {
-      total, minutes, seconds
-    };
-  }
-
-  const startTimer = (time) => {
-    setSongCurrentTime(0);
-
-    const timerInterval = setInterval(() => {
-      incrementSongTime();
-    }, 1000);
-
-    return timerInterval;
-  }
-
-  const incrementSongTime = () => {
-    setSongCurrentTime((songCurrentTime) => songCurrentTime + 1);
-  }
-
   // converts seconds to minutes and seconds string
   const convertToTime = (time) => {
     let { total, minutes, seconds } = getTimeRemaining(time);
@@ -52,11 +14,20 @@ function SongInformation(props) {
 
     return outputTime;
   }
+  
+  const getTimeRemaining = (e) => {
+    const total = e;
+    const minutes = Math.floor(total / 60);
+    const seconds = total - minutes * 60;
+    return {
+      total, minutes, seconds
+    };
+  }
 
   return (
     <div className={Styles.songInformation}>
       <div>Currently Playing: {props.isPlaying ? props.queue[0].Title : "nothing"}</div>
-      <div>{convertToTime(songCurrentTime)}/{convertToTime(songFullTime)}</div>
+      <div>{convertToTime(props.songCurrentTime)}/{convertToTime(props.songFullTime)}</div>
     </div>
   );
 }
