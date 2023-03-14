a481b11d1f12d1e667a067ad39936176cc65ba64
Tait Weinert <tait.weinert@outlook.com>
2023-03-14 18:54:08 +1030

    functional progress bar

--- src/components/Footer/SongProgressBar.js
+++ src/components/Footer/SongProgressBar.js
@@ -1,10 +1,21 @@
+import { useEffect } from "react";
 import Styles from "../../styles/footer/songProgressBar.module.css";
 
-// TODO add buttons
-function SongProgressBar() {
+// TODO interpolate bar progress
+function SongProgressBar(props) {
+  useEffect(() => {
+    updateProgressBar();
+  }, [props.songCurrentTime]);
+
+  const updateProgressBar = () => {
+    document.getElementById("bar-progress").style.width = ((props.songCurrentTime / props.songFullTime) * 100) + "%";
+  }
+  
   return(
     <div className={Styles.songProgressBar}>
-      ===============================================================
+      <div className={Styles.barBackground}>
+        <div className={Styles.barProgress} id="bar-progress"></div>
+      </div>
     </div>
   );
 }
